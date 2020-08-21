01c4e870ef3a942464cb768a40e9fac0a5daea16
Buzzaw91 <aleksi.ylonen2@gmail.com>
2020-08-21 21:45:54 +0300

    feat: handle signup & create jwt token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 const handleErrors = (err) => {
   console.log(err.message, err.code);
@@ -18,6 +19,14 @@ const handleErrors = (err) => {
   return errors;
 }
 
+const MAX_AGE_IN_SECONDS = 3 * 24 * 60 * 60;
+
+const createToken = (id) => {
+  return jwt.sign({ id }, 'a temporary secret...', {
+    expiresIn: MAX_AGE_IN_SECONDS
+  });
+}
+
 module.exports.signup_get = (req, res) => {
   res.render('signup');
 }
@@ -28,8 +37,10 @@ module.exports.signup_post = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const user = await User.create({ email, password })
-    res.status(201).json(user);
+    const user = await User.create({ email, password });
+    const token = createToken(user._id);
+    res.cookie('jwt', token, { httpOnly: true, maxAge: MAX_AGE_IN_SECONDS * 1000 });
+    res.status(201).json({ user: user._id });
   }
   catch (error) {
     const errors = handleErrors(error);
